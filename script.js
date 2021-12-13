881caee8a468c1392cada0123879fcbf74d4fd0a
Shubham <33shubham2021@gmail.com>
2021-12-13 15:15:42 +0530

    First Working Version

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function drawCircle(x , y , radius){
 }
 let n = 5;
 let distance = 200;
-let r = 200;
+let r = 250;
 let theta  = Math.PI / 2;
 let step  = (2*Math.PI) / n;
 
@@ -22,42 +22,32 @@ function drawEdges(n){
     var theta = Math.PI / 2;
     while (count < n){
         var x = r * Math.cos(theta) + center_x;
-        var y = r * Math.sin(theta) + center_y;
+        var y = center_y -  ( r * Math.sin(theta)) ;
         drawCircle(x , y , 10);
         theta += step;
         count++;
     }
 }
 
-drawEdges(5);
+drawEdges(n);
 
-// var x1 = center_x;
-// var y1 = center_y - distance;
-// var x2 = center_x + distance*Math.cos(Math.PI / 6);
-// var y2 = center_y + distance*Math.sin(Math.PI / 6);
-// var x3 = center_x - distance*Math.cos(Math.PI / 6);
-// var y3 = center_y + distance*Math.sin(Math.PI / 6);
-
-// drawCircle(x1 , y1 , 10);
-// drawCircle(x2 , y2 , 10);
-// drawCircle(x3 , y3 , 10);
-
-//window.requestAnimationFrame(draw);
 var x = center_x;
 var y = center_y;
 var itr = 0;
-var ratio = 61.8 / 100;
+var ratio = 61.8;
 function draw2(){
     if (itr > 10000) return;
-    drawCircle(x , y , 2);
+    drawCircle(x , y , 1);
     let temp = Math.floor(Math.random() * n);//a random number
     let step  = (2*Math.PI) / n;
     let theta  = (Math.PI / 2) + (step * temp);
     let X = r * Math.cos(theta) + center_x;
     let Y = center_y -  (r * Math.sin(theta)) ;
-
-    x  = (x+X) / ratio;
-    y = (y + Y) / ratio;
+    let a = ratio;let b = 100 - ratio;
+    
+    x  = (a*X + b*x) / (a+b);
+    y = (a*Y + b*y) / (a+b);
+    
     itr++;
     window.requestAnimationFrame(draw2);
 
@@ -65,29 +55,5 @@ function draw2(){
 
 draw2();            //Why some problem with draw(3 , 2) ...ie declaring n inside the function
 
-// function draw(){
-//     if (itr > 10000) return ;
-//     drawCircle(x , y , 2);
-//     var temp = Math.floor(Math.random() * 3);//a random no
-//     let X;
-//     let Y; 
-//     if (temp == 0){
-//         X = x1;
-//         Y = y1;
-//     }else if(temp == 1){
-//         X = x2;
-//         Y = y2;
-//     }else{
-//         X = x3;
-//         Y = y3;
-//     }
-
-//     x  = (x+X) / 2;
-//     y = (y + Y) / 2;
-//     itr++;
-//     window.requestAnimationFrame(draw);
-// }
-
-// draw();
 
 
