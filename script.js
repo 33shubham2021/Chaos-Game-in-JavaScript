6341cdc8a205fd76333d950f5fd2bea5af6e5a10
Shubham <33shubham2021@gmail.com>
2021-12-14 22:51:40 +0530

    HTML structures added

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+$(document).ready(function(){
+    $('.ui.accordion').accordion();
+});
+
+
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+// const left  =document.getElementById("left");
+// canvas.height = left.height;
+// canvas.width = left.width;
 
 var center_x = canvas.width / 2;
 var center_y = canvas.height / 2;
@@ -12,7 +21,7 @@ function drawCircle(x , y , radius){
 }
 let n = 5;
 let distance = 200;
-let r = 250;
+let r = 325;
 let theta  = Math.PI / 2;
 let step  = (2*Math.PI) / n;
 
@@ -54,3 +63,26 @@ function draw2(){
 }
 
 draw2();            //Why some problem with draw(3 , 2) ...ie declaring n inside the function
+
+function fetchDadJokes(){
+    var xhrRequest =new XMLHttpRequest();//creating request object
+
+    xhrRequest.onload  =function(){ //this is handler...function that will be called once we have a request
+        //converting the received String from server to JSON object
+        var response = JSON.parse(xhrRequest.response);
+        var joke  =response.attachments[0].text;
+        console.log(joke);
+
+        var jokeBox = document.getElementById('joke');
+        jokeBox.innerHTML = joke;
+    };
+
+    xhrRequest.open('get' , 'https://icanhazdadjoke.com/slack' , true);//opening the request
+    xhrRequest.send();
+
+}
+
+
+//$('fetch-button').click(fetchRandomDogImage);
+var button = document.getElementById('fetch-button');
+button.addEventListener('click' , fetchDadJokes);
