8d6eb21ae76e884d0163403965f29c10501190bd
Shubham <33shubham2021@gmail.com>
2021-12-15 10:17:15 +0530

    Added dynamic buttons

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ $(document).ready(function(){
     $('.ui.accordion').accordion();
 });
 
-
 window.alert("Try viewing the web page at 90% to 100% zoom for best experience");
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-// const left  =document.getElementById("left");
-// canvas.height = left.height;
-// canvas.width = left.width;
 
 var center_x = canvas.width / 2;
 var center_y = canvas.height / 2;
@@ -19,19 +16,18 @@ function drawCircle(x , y , radius){
     ctx.fill();
     ctx.fillStyle = 'cyan';
 }
-let n = 5;
-let distance = 200;
-let r = 325;
-let theta  = Math.PI / 2;
-let step  = (2*Math.PI) / n;
+var n = 3;
+var r = 325;
+var theta  = Math.PI / 2;
+var step  = (2*Math.PI) / n;
 
 function drawEdges(n){
-    var step  = (2*Math.PI) / n;
-    var count = 0;
-    var theta = Math.PI / 2;
+    let step  = (2*Math.PI) / n;
+    let count = 0;
+    let theta = Math.PI / 2;
     while (count < n){
-        var x = r * Math.cos(theta) + center_x;
-        var y = center_y -  ( r * Math.sin(theta)) ;
+        let x = r * Math.cos(theta) + center_x;
+        let y = center_y -  ( r * Math.sin(theta)) ;
         drawCircle(x , y , 10);
         theta += step;
         count++;
@@ -40,12 +36,15 @@ function drawEdges(n){
 
 drawEdges(n);
 
+var request;
 var x = center_x;
 var y = center_y;
 var itr = 0;
-var ratio = 61.8;
-function draw2(){
-    if (itr > 10000) return;
+var isClicked = false;
+var ratio = 50;
+
+function draw(x , y , itr ){
+    if (itr > 1000 ) return;
     drawCircle(x , y , 1);
     let temp = Math.floor(Math.random() * n);//a random number
     let step  = (2*Math.PI) / n;
@@ -58,11 +57,56 @@ function draw2(){
     y = (a*Y + b*y) / (a+b);
     
     itr++;
-    window.requestAnimationFrame(draw2);
+    draw(x , y , itr);
+}
+
+//window.setInterval(draw(x , y , itr) , 100);
+
+function draw2(){
+    if (itr > 5000 ) return;
+    drawCircle(x , y , 1);
+    let temp = Math.floor(Math.random() * n);//a random number
+    let step  = (2*Math.PI) / n;
+    let theta  = (Math.PI / 2) + (step * temp);
+    let X = r * Math.cos(theta) + center_x;
+    let Y = center_y -  (r * Math.sin(theta)) ;
+    let a = ratio;let b = 100 - ratio;
+    
+    x  = (a*X + b*x) / (a+b);
+    y = (a*Y + b*y) / (a+b);
+    
+    itr++;
+    request = window.requestAnimationFrame(draw2);
 
 }
 
 draw2();            //Why some problem with draw(3 , 2) ...ie declaring n inside the function
+ var primaryButton = document.getElementById('ui primary button');
+ 
+
+function onClick(){
+    //window.cancelAnimationFrame(request);
+    colorRect(0,0, canvas.width,canvas.height, 'black');//ctx.clearRect(0,0,canvas.width,canvas.height);
+    
+    n = document.getElementById('nthsidedpolygon').value;
+    ratio = document.getElementById('distratio').value;
+    //drawEdges(n);
+    //x = center_x;
+    //y = center_y;
+    //itr = 0;
+    //draw2();
+    //return true;
+}
+
+ 
+ ;
+// function playForSide(){
+//     drawEdges(n);
+//     draw2()
+// }
+
+
+
 
 function fetchDadJokes(){
     var xhrRequest =new XMLHttpRequest();//creating request object
@@ -71,10 +115,10 @@ function fetchDadJokes(){
         //converting the received String from server to JSON object
         var response = JSON.parse(xhrRequest.response);
         var joke  =response.attachments[0].text;
-        console.log(joke);
+        window.alert(joke);
 
-        var jokeBox = document.getElementById('joke');
-        jokeBox.innerHTML = joke;
+        // var jokeBox = document.getElementById('joke');
+        // jokeBox.innerHTML = joke;
     };
 
     xhrRequest.open('get' , 'https://icanhazdadjoke.com/slack' , true);//opening the request
@@ -86,3 +130,5 @@ function fetchDadJokes(){
 //$('fetch-button').click(fetchRandomDogImage);
 var button = document.getElementById('fetch-button');
 button.addEventListener('click' , fetchDadJokes);
+
+
